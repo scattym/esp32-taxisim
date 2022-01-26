58f14e7ff09a19c89b341a59fdf27d32123c9f46
matt <mattjclark0407@hotmail.com>
2022-01-26 13:27:08 +1100

    Change dummy data being used.

--- components/cardsim/cardsim.c
+++ components/cardsim/cardsim.c
@@ -4,7 +4,7 @@
 #include <cardsim.h>
 
 unsigned char* make_card_read(unsigned int *len) {
-    unsigned char cardReadB64[] = "***REMOVED***";
+    unsigned char cardReadB64[] = "AiUgIF5BQkNERUZHSEkgSktMTU5PUCBNUi5eXj87NjAwNzU1NTEwMDUwMDQ2MTE1Nz05OTk5MTk3NzA3MDQ9PysgICAgICAgICAgICAgMzEwMCAgICAgICAgICAgIDEgICAgICAgICAgICA1ODAwNTU2MyAgMDAxMDEgICAgICAgICAgICAgICAgICAgICA/DQMK";
 
     unsigned char* cardReadBytes = base64_decode(cardReadB64, 196, len);
     return cardReadBytes;
